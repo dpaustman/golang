3216048c94757eef7377679cfae972e5c7d57a64
dpaustman <ljw0216@outlook.com>
2018-08-05 19:24:40 +0800

    study queue

---
+++ day05/queue/Queue.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type Queue []int
+
+func (q *Queue) Push(v int) {
+	*q = append(*q, v)
+}
+
+func (q *Queue) Pop() int {
+	head := (*q)[0]
+	*q = (*q)[1:]
+	return head
+}
+
+func (q *Queue) IsEmpty() bool {
+	return len(*q) == 0
+}
+
+func main() {
+	q := Queue{1}
+	q.Push(4)
+	q.Push(6)
+	fmt.Println(q.Pop())
+	fmt.Println(q.Pop())
+	fmt.Println((q.IsEmpty()))
+	fmt.Println(q.Pop())
+	fmt.Println(q.IsEmpty())
+}
