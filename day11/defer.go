6037a6155560668253b6a39c224366e990ebddf3
dpaustman <ljw0216@outlook.com>
2018-08-13 23:18:13 +0800

    study panic/defer

---
+++ day11/defer.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"os"
+	"io"
+)
+
+func CopyFile(dst, src string) (w int64, err error) {
+	srcFile, err := os.Open(src)
+	if err != nil {
+		return
+	}
+	defer srcFile.Close()
+
+	dstFile, err := os.Create(dstName)
+	if err != nil {
+		return
+	}
+	defer dstFile.Close()
+	return io.Copy(dstFile, srcFile)
+}
+
+func main() {
+	
+}
